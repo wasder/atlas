df6c310f65be6373fd556e303f7e8156ce0997d3
Goncharov Ivan <iagonch9@mts.ru>
2022-05-24 18:19:55 +0300

    jdk 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,8 @@ configure(subprojects) {
     apply(plugin = "io.spring.dependency-management")
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_15
-        targetCompatibility = JavaVersion.VERSION_15
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     tasks.compileJava {
